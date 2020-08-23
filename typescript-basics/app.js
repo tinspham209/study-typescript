9a9611a397cdf88c047313945f0dc9189b5d6dbd
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-23 16:48:22 +0700

    Literal Types

--- typescript-basics/app.js
+++ typescript-basics/app.js
@@ -46,6 +46,7 @@ var enumType = {
     hobbies: ["Sport", "Cooking"],
     role: Role.ADMIN
 };
+// Union Types
 function combine(input1, input2) {
     var result;
     if (typeof input1 === "number" && typeof input2 === "number") {
@@ -60,3 +61,17 @@ var combinedAges = combine(30, 26);
 console.log(combinedAges);
 var combinedNames = combine("A", "B");
 console.log(combinedNames);
+// Literal Types
+function combineLiteral(input1, input2, resultConversion) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combinedStringAges = combineLiteral("30", "26", "as-number");
+console.log("combinedStringAges ", combinedStringAges);
--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -92,3 +92,24 @@ console.log(combinedAges);
 
 const combinedNames = combine("A", "B");
 console.log(combinedNames);
+
+// Literal Types
+function combineLiteral(
+	input1: number | string,
+	input2: number | string,
+	resultConversion: "as-number" | "as-text"
+) {
+	let result;
+	if (
+		(typeof input1 === "number" && typeof input2 === "number") ||
+		resultConversion === "as-number"
+	) {
+		result = +input1 + +input2;
+	} else {
+		result = input1.toString() + input2.toString();
+	}
+	return result;
+}
+
+const combinedStringAges = combineLiteral("30", "26", "as-number");
+console.log("combinedStringAges ", combinedStringAges);
