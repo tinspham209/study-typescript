b177ef6d6d3c7ac0ae964e2d8c36e7cae710817b
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-23 16:25:25 +0700

    Union Types

--- typescript-basics/app.js
+++ typescript-basics/app.js
@@ -46,3 +46,17 @@ var enumType = {
     hobbies: ["Sport", "Cooking"],
     role: Role.ADMIN
 };
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine("A", "B");
+console.log(combinedNames);
