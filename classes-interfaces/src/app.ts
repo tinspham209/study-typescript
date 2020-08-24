3c06dc0e9d4de82e27c61c083859d23912704448
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-24 16:31:22 +0700

    "private" & "public" access modifiers

--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-	name: string;
+	public name: string;
+	private employees: string[] = [];
 
 	constructor(n: string) {
 		this.name = n;
@@ -8,12 +9,27 @@ class Department {
 	describe(this: Department) {
 		console.log("Department: " + this.name);
 	}
+
+	addEmployee(employee: string) {
+		this.employees.push(employee);
+	}
+
+	printEmployeeInformation() {
+		console.log(this.employees.length);
+		console.log(this.employees);
+	}
 }
 
 const accounting = new Department("Accounting");
 
 console.log(accounting);
 
-const accountingCopy = { name: "Dummy", describe: accounting.describe };
+// const accountingCopy = { name: "Dummy", describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Mamu");
+
+accounting.describe();
+accounting.printEmployeeInformation();
